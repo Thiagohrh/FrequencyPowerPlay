aaa13c970d787a2c628d048694c5ca14aeb840ee
Thiago Henrique <thiagohrh@gmail.com>
2019-10-04 19:17:07 -0300

    Added a few tracks and a random music selector.

---
+++ FrequencyPowerPlay/Assets/Scripts/MusicSelector.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public class MusicSelector : MonoBehaviour {
+
+    public AudioClip[] musics;
+    private AudioSource audioSource;
+	void Start () 
+    {
+        audioSource = GetComponent<AudioSource>();
+        SelectRandomTrack();
+	}
+
+    void SelectRandomTrack() 
+    {
+        audioSource.Stop();
+        int randIndex = Random.Range(0, musics.Length - 1);
+        audioSource.clip = musics[randIndex];
+        audioSource.Play();
+    }
+}
