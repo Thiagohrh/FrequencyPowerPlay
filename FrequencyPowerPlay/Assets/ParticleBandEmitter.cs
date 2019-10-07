25f88fb86287c7f74af5563b3ef7864772d5c6c1
Thiago Henrique <thiagohrh@gmail.com>
2019-10-06 21:39:06 -0300

    Improved beat detection. Added ugly particles.

---
+++ FrequencyPowerPlay/Assets/ParticleBandEmitter.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(ParticleSystem))]
+public class ParticleBandEmitter : MonoBehaviour {
+    private ParticleSystem particleSystem;
+    public int bandIndex = 1;
+    //Controls the sensitivity in order to check for a beat
+    [Header("Beat Sensitivity values")]
+    public float thresholdSensitivity = 0.5f;
+    private float previousAudioValue = 0.0f;
+    private float currentAudioValue = 0.0f;
+    //Time control variables.
+    public float timeBetweenBeats = 1.0f;
+    private float beatTimer = 0.0f;
+    void Start () {
+        particleSystem = GetComponent<ParticleSystem>();
+	}
+	
+	void Update () {
+        previousAudioValue = currentAudioValue;
+        currentAudioValue = AudioSpecterSampler.frequencyBand[bandIndex];
+
+        //if (previousAudioValue > thresholdSensitivity &&
+        //    currentAudioValue <= thresholdSensitivity &&
+        //    beatTimer <= 0.0f)
+        //{
+        //    particleSystem.Play();
+        //    beatTimer = timeBetweenBeats;
+        //}
+        //if (previousAudioValue <= thresholdSensitivity &&
+        //    currentAudioValue > thresholdSensitivity &&
+        //    beatTimer <= 0.0f)
+        //{
+        //    particleSystem.Play();
+        //    beatTimer = timeBetweenBeats;
+        //}
+        if (currentAudioValue - previousAudioValue > thresholdSensitivity &&
+            currentAudioValue - previousAudioValue > 0)
+        {
+            particleSystem.Play();
+            beatTimer = timeBetweenBeats;
+        }
+
+        if (beatTimer > 0.0f)
+        {
+            beatTimer -= Time.deltaTime;
+        }
+    }
+}
--- FrequencyPowerPlay/Assets/VideoClipChanger.cs
+++ FrequencyPowerPlay/Assets/VideoClipChanger.cs
@@ -4,16 +4,19 @@ using UnityEngine.Video;
 [RequireComponent(typeof(VideoPlayer))]
 public class VideoClipChanger : MonoBehaviour
 {
-
     private VideoPlayer videoPlayer;
     public VideoClip[] videoSources;
     private int lastVideoIndex = -1;
+    //Controls which band this script is going to listen to.
     public int bandIndex = 0;
+    //Controls the sensitivity in order to check for a beat
+    [Header("Beat Sensitivity values")]
     public float thresholdSensitivity = 0.5f;
-    public float timeBetweenBeats = 2.0f;
-
+    private float previousAudioValue = 0.0f;
+    private float currentAudioValue = 0.0f;
+    //Time control variables.
+    public float timeBetweenBeats = 1.0f;
     private float beatTimer = 0.0f;
-
 
     void Start()
     {
@@ -23,8 +26,19 @@ public class VideoClipChanger : MonoBehaviour
 
     void Update()
     {
+        previousAudioValue = currentAudioValue;
+        currentAudioValue = AudioSpecterSampler.frequencyBand[bandIndex];
 
-        if (AudioSpecterSampler.frequencyBand[bandIndex] > thresholdSensitivity && beatTimer <= 0.0f)
+        if (previousAudioValue > thresholdSensitivity && 
+            currentAudioValue <= thresholdSensitivity &&
+            beatTimer <= 0.0f)
+        {
+            SelectRandomVideo();
+            beatTimer = timeBetweenBeats;
+        }
+        if (previousAudioValue <= thresholdSensitivity &&
+            currentAudioValue > thresholdSensitivity &&
+            beatTimer <= 0.0f)
         {
             SelectRandomVideo();
             beatTimer = timeBetweenBeats;
@@ -38,6 +52,7 @@ public class VideoClipChanger : MonoBehaviour
 
     private void SelectRandomVideo()
     {
+        Debug.Log("BOOM!");
         videoPlayer.Stop();
         int randIndex = Random.Range(0, videoSources.Length - 1);
         if (randIndex == lastVideoIndex)
