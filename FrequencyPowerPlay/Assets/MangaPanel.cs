58b67357b4d7d0ae3ca898b22209b01c42160bc0
RedTarantula <joaopedrothmann@hotmail.com>
2019-10-09 11:11:15 -0300

    Started adding manga panel movement

---
+++ FrequencyPowerPlay/Assets/MangaPanel.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MangaPanel : MonoBehaviour {
+
+    public int step = 0;
+    public int stepsMax;
+    public Animator anim;
+
+    void Awake()
+    {
+        if (anim == null) 
+        anim = GetComponent<Animator>();
+    }
+
+    public bool Click(float speed =-1)
+    {
+        Debug.Log("Step");
+        step++;
+        anim.SetInteger("Step",step);
+        anim.SetTrigger("StepT");
+        if(speed >0)
+        anim.SetFloat("AnimationSpeed",speed);
+        bool b = step >= stepsMax;
+
+        if(b)
+        {
+            step = 0;
+        }
+        return b;
+    }
+}
--- FrequencyPowerPlay/Assets/VideoClipChanger.cs
+++ FrequencyPowerPlay/Assets/VideoClipChanger.cs
@@ -1,22 +1,28 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 using UnityEngine.Video;
 
 [RequireComponent(typeof(VideoPlayer))]
 public class VideoClipChanger : MonoBehaviour
 {
-    private VideoPlayer videoPlayer;
+    [Header("Videos")]
+    public VideoPlayer videoPlayer;
     public VideoClip[] videoSources;
-    private int lastVideoIndex = -1;
+    [Header("Manga Panels")]
+    public MangaPanel[] panels;
+    public int panelIndex = 0;
+    public int lastVideoIndex = -1;
     //Controls which band this script is going to listen to.
     public int bandIndex = 0;
     //Controls the sensitivity in order to check for a beat
     [Header("Beat Sensitivity values")]
     public float thresholdSensitivity = 0.5f;
-    private float previousAudioValue = 0.0f;
-    private float currentAudioValue = 0.0f;
+    public float previousAudioValue = 0.0f;
+    public float currentAudioValue = 0.0f;
     //Time control variables.
     public float timeBetweenBeats = 1.0f;
-    private float beatTimer = 0.0f;
+    public float beatTimer = 0.0f;
+
 
     void Start()
     {
@@ -34,6 +40,7 @@ public class VideoClipChanger : MonoBehaviour
             beatTimer <= 0.0f)
         {
             SelectRandomVideo();
+            MangaPanelClick();
             beatTimer = timeBetweenBeats;
         }
         if (previousAudioValue <= thresholdSensitivity &&
@@ -41,6 +48,7 @@ public class VideoClipChanger : MonoBehaviour
             beatTimer <= 0.0f)
         {
             SelectRandomVideo();
+            MangaPanelClick();
             beatTimer = timeBetweenBeats;
         }
 
@@ -50,16 +58,21 @@ public class VideoClipChanger : MonoBehaviour
         }
     }
 
+    private void MangaPanelClick()
+    {
+        Debug.Log(panels[panelIndex].Click());
+    }
+
     private void SelectRandomVideo()
     {
         //Debug.Log("BOOM!");
         videoPlayer.Stop();
-        int randIndex = Random.Range(0, videoSources.Length - 1);
+        int randIndex = UnityEngine.Random.Range(0, videoSources.Length - 1);
         if (randIndex == lastVideoIndex)
         {
             do
             {
-                randIndex = Random.Range(0, videoSources.Length - 1);
+                randIndex = UnityEngine.Random.Range(0, videoSources.Length - 1);
             } while (randIndex == lastVideoIndex);
         }
         lastVideoIndex = randIndex;
